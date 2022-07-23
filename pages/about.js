445d806e40a800714798bdadf2f9a03a91318c90
sL0p3 <shlokjakhotia@gmail.com>
2022-07-23 20:00:04 +0530

    images and metadata

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,17 @@
+import Head from 'next/head'
 const About = () => {
     return (
-      <div>
-        <h1>About</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
-      </div>
+        <>
+            <Head>
+                <title>Ninja List | About</title>
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
+            </div>
+      </>
     );
   }
    
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,19 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>HomePage</h1>
-      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto voluptates ipsum in autem aspernatur repellat laboriosam voluptatem eum explicabo facilis voluptate, at aliquam facere ducimus ratione, quas eaque id expedita, tempora saepe. Architecto.</p>
-      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit non assumenda mollitia veniam veritatis, optio, nisi provident unde, modi sint sequi corporis porro magnam cumque saepe corrupti ducimus tenetur eos enim! Autem, aperiam at!</p>
-      <Link href = "/ninjas"><a className={styles.btn}>See Ninja Listing</a></Link>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+      </Head>
+      <div>
+        <h1 className={styles.title}>HomePage</h1>
+        <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto voluptates ipsum in autem aspernatur repellat laboriosam voluptatem eum explicabo facilis voluptate, at aliquam facere ducimus ratione, quas eaque id expedita, tempora saepe. Architecto.</p>
+        <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit non assumenda mollitia veniam veritatis, optio, nisi provident unde, modi sint sequi corporis porro magnam cumque saepe corrupti ducimus tenetur eos enim! Autem, aperiam at!</p>
+        <Link href = "/ninjas"><a className={styles.btn}>See Ninja Listing</a></Link>
+      </div>
+    </>
   )
 }
